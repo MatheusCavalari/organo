aca4f309129fe1fe6870ce94528251fcee6e0344
Matheus Cavalari <matheuscavbarbosa@hotmail.com>
2024-04-23 16:12:13 -0300

    Update values

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -2,34 +2,62 @@ import "./Formulario.css";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import Botao from "../Botao";
+import React, { useState } from 'react';
+
 
 const Formulario = () => {
   const times = [
-    'Programação',
-    'Front-End',
-    'Data Science',
-    'Devops',
-    'UX e Design',
-    'Mobile',
-    'Inovação e Gestão'
-  ]
+    "Programação",
+    "Front-End",
+    "Data Science",
+    "Devops",
+    "UX e Design",
+    "Mobile",
+    "Inovação e Gestão",
+  ];
 
-  const aoSalvar = (evento) =>{
-    evento.preventDefault()
-    console.log('Envio do form');
-  }
-  
+  const aoSalvar = (evento) => {
+    evento.preventDefault();
+    console.log("Envio do form " ,nome, cargo, imagem, time );
+  };
+
+  const [nome, setNome] = useState('');
+  const [cargo, setCargo] = useState('');
+  const [imagem, setImagem] = useState('');
+  const [time, SetTime] = useState('');
+
   return (
     <section className="formulario">
-      <form onSubmit = {aoSalvar} >
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto obrigatorio={true }label="Cargo" placeholder="Digite seu cargo" />
-        <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-        <ListaSuspensa obrigatorio={true} label="Time" itens = {times} />
-        <Botao>
-          Criar Card
-        </Botao>
+        <CampoTexto
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite seu nome"
+          valor={nome}
+          aoAlterado={valor => setNome(valor)}
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Cargo"
+          placeholder="Digite seu cargo"
+          valor={cargo}
+          aoAlterado={valor => setCargo(valor)}
+        />
+        <CampoTexto 
+          label="Imagem" 
+          placeholder="Digite o endereço da imagem"
+          valor={imagem}
+          aoAlterado={valor => setImagem(valor)}
+          />
+        <ListaSuspensa 
+          obrigatorio={true} 
+          label="Time" 
+          itens={times}
+          valor={time}
+          aoAlterado={valor => SetTime(valor)}
+          />
+        <Botao>Criar Card</Botao>
       </form>
     </section>
   );
--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -1,11 +1,19 @@
 import "./ListaSuspensa.css";
 
 const ListaSuspensa = (props) => {
+  
+  const aoSelecionado = (evento) =>{
+    props.aoAlterado(evento.target.value)
+  };
+
   console.log(props);
   return (
     <div className="lista-suspensa">
       <label> {props.label} </label>
-      <select required={props.obrigatorio}>
+      <select 
+      required={props.obrigatorio}
+      onChange={aoSelecionado}
+      >
         {props.itens.map((item) => {
           return <option key={item}>{item}</option>;
         })}
